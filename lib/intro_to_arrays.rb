097de3654c18a0df692259df9f6f8dd094f6d581
Andrew Sprague <andrewsprague@outlook.com>
2018-08-12 15:55:40 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+    array = []
+end
+
+def array_with_two_elements
+  array = [1,2]
+end
+
+def first_element (array)
+  array.first
+end
+
+def third_element (array)
+  array[2]
+end
+
+def last_element (array)
+  
+  array.last
+
+end
+
+def first_element_with_array_methods (array)
+  array.shift()
+end
+
+def last_element_with_array_methods(array)
+  array.pop()
+end
+
+def length_of_array (array)
+  array.count()
+end
